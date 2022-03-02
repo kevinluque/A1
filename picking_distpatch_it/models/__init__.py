59da823e9c89e7476063bd75b8b7c2037123a655
kevinluque <54952077+kevinluque@users.noreply.github.com>
2022-03-02 13:53:42 -0500

    Delete picking_distpatch_it directory

--- picking_distpatch_it/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import picking_distpatch
-from . import fleet_vehicle
--- picking_distpatch_it/models/fleet_vehicle.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-from odoo import _
-
-class FleetVehicle(models.Model):
-    _inherit = 'fleet.vehicle'
-
-    weight_vehicle = fields.Float('Carga del vehiculo')
