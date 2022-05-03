5dfe902c97898ab35f6cac5470317742a663bb04
kevinluque <54952077+kevinluque@users.noreply.github.com>
2022-05-03 10:49:22 -0500

    Add files via upload

---
+++ res_partner_filtro_nombre_comerc/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'Contacto nombre comercial',
+	'category': 'base',
+	'author': 'ITGRUPO',
+ 	'depends': ['base','res_partner_clase_categoria_fields'],
+	'version': '1.0',
+	'description':"""
+		Contacto nombre comercial
+	""",
+	'auto_install': False,
+	'demo': [],
+	'data':	[
+    	'views/res_partner_name_comercial.xml',
+	],
+	'installable': True
+}
---
+++ res_partner_filtro_nombre_comerc/models/res_partner_name_comercial.py
@@ -0,0 +1,2 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError
